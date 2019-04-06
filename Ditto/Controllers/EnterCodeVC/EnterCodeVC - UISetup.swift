da5f633b211d013a4b8989f5bc31bbf62fd548b7
Candace Chiang <chiang.candace@berkeley.edu>
2019-04-06 15:51:36 -0700

    new

--- Ditto/Controllers/EnterCodeVC/EnterCodeVC - UISetup.swift
+++ Ditto/Controllers/EnterCodeVC/EnterCodeVC - UISetup.swift
@@ -11,31 +11,33 @@ import UIKit
 extension EnterCodeViewController {
     func setUpLabels() {
         tagLabel = UILabel(frame: CGRect(x: 0, y: 0, width: view.frame.width * 2/3, height: view.frame.height/10))
-        tagLabel.center = CGPoint(x: view.frame.width/2, y: view.frame.height/3)
+        tagLabel.center = CGPoint(x: view.frame.width/2, y: view.frame.height/5)
+        tagLabel.textAlignment = .center
         tagLabel.text = "find a playlist"
-        tagLabel.font = UIFont(name: "Roboto-Light", size: 17)
+        tagLabel.font = UIFont(name: "Roboto-Light", size: 28)
         view.addSubview(tagLabel)
     }
     
     func setUpInteractive() {
-        codeInput = UITextField(frame: CGRect(x: 0, y: 0, width: view.frame.width * 3/4, height: view.frame.height/8))
-        codeInput.center = CGPoint(x: view.frame.width/2, y: view.frame.height/2)
-        codeInput.font = UIFont(name: "Roboto-Regular", size: 16)
+        codeInput = UITextField(frame: CGRect(x: 0, y: 0, width: view.frame.width * 2/3, height: view.frame.height/11))
+        codeInput.center = CGPoint(x: view.frame.width/2, y: view.frame.height/3)
+        codeInput.font = UIFont(name: "Roboto-Bold", size: 28)
         codeInput.textAlignment = .center
         codeInput.placeholder = "PLAYLIST CODE"
         codeInput.textColor = UIColor(hexString: "#BF95DC")
+        codeInput.layer.borderWidth = 2.0
         codeInput.layer.borderColor = UIColor(hexString: "#BF95DC").cgColor
         codeInput.layer.cornerRadius = 7.0
         codeInput.keyboardType = UIKeyboardType.numberPad
         codeInput.addTarget(self, action: #selector(codeEntered), for: .allEditingEvents)
         view.addSubview(codeInput)
         
-        searchButton = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.width/4, height: view.frame.height/15))
-        searchButton.center = CGPoint(x: view.frame.width/2, y: view.frame.height * 2/3)
+        searchButton = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.width/3, height: view.frame.height/22))
+        searchButton.center = CGPoint(x: view.frame.width/2, y: view.frame.height/2)
         searchButton.setTitle("Search", for: .normal)
         searchButton.setTitleColor(.white, for: .normal)
         searchButton.backgroundColor = UIColor(hexString: "#7383C5")
-        searchButton.titleLabel!.font = UIFont(name: "Roboto-Bold", size: 13)
+        searchButton.titleLabel!.font = UIFont(name: "Roboto-Bold", size: 20)
         searchButton.layer.cornerRadius = 5.0
         searchButton.addTarget(self, action: #selector(filter), for: .touchUpInside)
         view.addSubview(searchButton)
