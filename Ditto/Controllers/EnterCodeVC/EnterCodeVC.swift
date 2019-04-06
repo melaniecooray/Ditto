17ecb45c1bcf9896718938c0fcf329509c11a43d
Candace Chiang <chiang.candace@berkeley.edu>
2019-04-06 15:16:39 -0700

    uh

---
+++ Ditto/Controllers/EnterCodeVC/EnterCodeVC.swift
@@ -0,0 +1,62 @@
+//
+//  EnterCodeVC.swift
+//  Ditto
+//
+//  Created by Candace Chiang on 4/6/19.
+//  Copyright © 2019 Melanie Cooray. All rights reserved.
+//
+
+import UIKit
+
+class EnterCodeViewController: UIViewController, UITextFieldDelegate {
+
+    var tagLabel: UILabel!
+    var codeInput: UITextField!
+    var searchButton: UIButton!
+    
+    var code = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setUpLabels()
+        setUpInteractive()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.codeInput.delegate = self
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    //dismiss keyboard when you press return
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    @objc func codeEntered(_ sender: UITextField) {
+        let possCode = sender.text
+        if possCode != "" {
+            code = possCode!
+        }
+    }
+    
+    @objc func filter(_ sender: UIButton) {
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Ditto/Controllers/PreviewPlaylistVC/PreviewPlaylistVC - UISetup.swift
@@ -0,0 +1,13 @@
+//
+//  PreviewPlaylistVC - UISetup.swift
+//  Ditto
+//
+//  Created by Candace Chiang on 4/6/19.
+//  Copyright © 2019 Melanie Cooray. All rights reserved.
+//
+
+import UIKit
+
+extension PreviewPlaylistViewController {
+    
+}
