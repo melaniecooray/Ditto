1a6f4d4dcc9dc2e5dd02a457d6257729b57c5b70
Melanie Cooray <cooraymelanie@gmail.com>
2019-04-06 14:30:25 -0700

    login and sign up screens + signup to firebase

--- Ditto/Controllers/LoginVC/LoginVC - UISetup.swift
+++ Ditto/Controllers/LoginVC/LoginVC - UISetup.swift
@@ -12,12 +12,22 @@ import SkyFloatingLabelTextField
 extension LoginViewController {
     
     func initUI() {
+        setupLogo()
         setupEmailTextField()
         setupPasswordTextField()
+        setupLoginButton()
+        setupSignUp()
+    }
+    
+    func setupLogo() {
+        logo = UIImageView(frame: CGRect(x: 0, y: 0, width: 300, height: 300))
+        logo.center = CGPoint(x: view.frame.width/2, y: 200)
+        logo.image = UIImage(named: "logo")
+        view.addSubview(logo)
     }
     
     func setupEmailTextField() {
-        emailTextField = SkyFloatingLabelTextField(frame: CGRect(x: 30, y: 100, width: view.frame.width - 100, height: 100))
+        emailTextField = SkyFloatingLabelTextField(frame: CGRect(x: 50, y: logo.frame.maxY - 50, width: view.frame.width - 100, height: 100))
         emailTextField.placeholder = "Email"
         emailTextField.title = "Email Address"
         emailTextField.errorColor = UIColor.red
@@ -26,12 +36,33 @@ extension LoginViewController {
     }
     
     func setupPasswordTextField() {
-        passwordTextField = SkyFloatingLabelTextField(frame: CGRect(x: 30, y: 200, width: view.frame.width - 100, height: 100))
+        passwordTextField = SkyFloatingLabelTextField(frame: CGRect(x: 50, y: emailTextField.frame.maxY + 10, width: view.frame.width - 100, height: 100))
         passwordTextField.placeholder = "Password"
         passwordTextField.title = "Password"
         passwordTextField.errorColor = UIColor.red
         passwordTextField.addTarget(self, action: #selector(passwordTextFieldDidChange(_:)), for: .editingChanged)
         self.view.addSubview(passwordTextField)
+    }
+    
+    func setupLoginButton() {
+        loginButton = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.width/2, height: 50))
+        loginButton.center = CGPoint(x: view.frame.width/2, y: passwordTextField.frame.maxY + 50)
+        loginButton.setTitle("Login", for: .normal)
+        loginButton.layer.cornerRadius = 10
+        loginButton.backgroundColor = UIColor(hexString: "7383C5")
+        view.addSubview(loginButton)
+    }
+    
+    func setupSignUp() {
+        signUpText = UILabel(frame: CGRect(x: 0, y: 0, width: view.frame.width/2, height: 50))
+        signUpText.center = CGPoint(x: view.frame.width/2 - 25, y: loginButton.frame.maxY + 50)
+        signUpText.text = "Don't have an account?"
+        view.addSubview(signUpText)
+        signUpButton = UIButton(frame: CGRect(x: view.frame.width/2 - 5, y: signUpText.frame.minY, width: 200, height: 50))
+        signUpButton.setTitle("Sign Up!", for: .normal)
+        signUpButton.setTitleColor(UIColor(hexString: "7383C5"), for: .normal)
+        signUpButton.addTarget(self, action: #selector(toSignUp), for: .touchUpInside)
+        view.addSubview(signUpButton)
     }
     
     // This will notify us when something has changed on the textfield
@@ -61,6 +92,10 @@ extension LoginViewController {
                 }
             }
         }
+    }
+    
+    @objc func toSignUp() {
+        performSegue(withIdentifier: "toSignUp", sender: self)
     }
     
 }
--- Ditto/Controllers/LoginVC/LoginVC.swift
+++ Ditto/Controllers/LoginVC/LoginVC.swift
@@ -11,12 +11,44 @@ import SkyFloatingLabelTextField
 
 class LoginViewController: UIViewController {
     
+    var logo: UIImageView!
+    
     var emailTextField : SkyFloatingLabelTextField!
     var passwordTextField : SkyFloatingLabelTextField!
+    
+    var loginButton: UIButton!
+    
+    var signUpText: UILabel!
+    var signUpButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         initUI()
+        addTapDismiss()
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    func addTapDismiss() {
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard)))
+    }
+    @objc func dismissKeyboard() {
+        emailTextField.resignFirstResponder()
+        passwordTextField.resignFirstResponder()
+    }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0 {
+                self.view.frame.origin.y -= 200
+            }
+        }
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
     }
 
 }
