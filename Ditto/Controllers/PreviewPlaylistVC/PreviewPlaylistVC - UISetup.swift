02e8699f8b02d0d60c7bdcdc4ffdfffd7ceb194e
Candace Chiang <chiang.candace@berkeley.edu>
2019-04-06 18:19:05 -0700

    buttons

--- Ditto/Controllers/PreviewPlaylistVC/PreviewPlaylistVC - UISetup.swift
+++ Ditto/Controllers/PreviewPlaylistVC/PreviewPlaylistVC - UISetup.swift
@@ -9,5 +9,19 @@
 import UIKit
 
 extension PreviewPlaylistViewController {
+    func setUpBackground() {
+        colorBlock = UIView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height * 4/5))
+        colorBlock.backgroundColor = UIColor(hexString: "#7383C5")
+        view.addSubview(colorBlock)
+    }
     
+    func setUpLabels() {
+        nameLabel = UILabel(frame: CGRect(x: 0, y: 0, width: view.frame.width * 2/3, height: view.frame.height/15))
+        //nameLabel.center = CGPoint
+        nameLabel.font = UIFont(name: "Roboto-Bold", size: 15)
+        nameLabel.text = "vibe station"
+        nameLabel.textColor = .white
+        view.addSubview(nameLabel)
+        
+    }
 }
